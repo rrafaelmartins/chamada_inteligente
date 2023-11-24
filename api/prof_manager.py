ac5a11de890a478b2e0ee1f635f0cde107b4270b
MateusFerreiraMachado <mateusferreira@id.uff.br>
2023-11-24 20:40:18 -0300

    Resolvendo bugs de conflito

---
+++ api/prof_manager.py
@@ -0,0 +1,298 @@
+from flask import request, jsonify, Blueprint
+import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
+password = os.getenv('PASSWORD')
+
+professor_blueprint = Blueprint('professor_blueprint', __name__)
+
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password=f'{password}',
+    database='intelitestenovo'
+)
+
+def open_conexao():
+    conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password=f'{password}',
+    database='intelitestenovo'
+)
+    return conexao
+
+@professor_blueprint.route('/get_turmas_prof/<string:id_professor>', methods=['GET'])
+def get_turmas(id_professor: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f'select nome, codigo_turma, id_turma FROM Disciplina JOIN Turma ON Turma.id_disciplina = Disciplina.id_disciplina WHERE id_professor = {id_professor}; '
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+@professor_blueprint.route('/get_nomedisciplina_by_turmaid/<string:id_turma>', methods=['GET'])
+def get_nomedisciplina_by_turmaid(id_turma: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""SELECT Disciplina.nome
+                FROM Turma
+                JOIN Disciplina ON Turma.id_disciplina = Disciplina.id_disciplina
+                WHERE Turma.id_turma = {id_turma};"""
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+@professor_blueprint.route('/get_historico/<string:id_turma>', methods=['GET']) #MUDAR PARA GET_CHAMADA !!!!!!!!! NOME ERRADO !!!!!
+def get_historico(id_turma: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""SELECT Aluno.matricula, Aluno.primeiro_nome, Aluno.segundo_nome
+                FROM Inscricao
+                JOIN Aluno ON Inscricao.id_aluno = Aluno.id_aluno
+                WHERE Inscricao.id_turma = {id_turma};"""
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+@professor_blueprint.route('/get_datas_historico_prof/<string:id_turma>', methods=['GET'])
+def get_datas_historico_prof(id_turma: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""SELECT DATE_FORMAT(data_hora_inicio, '%d/%m/%Y') AS data_inicio_formatada
+                FROM Aula
+                WHERE id_turma = {id_turma} AND data_hora_inicio <= NOW();"""
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+@professor_blueprint.route('/get_nome_prof/<string:id_prof>', methods=['GET'])
+def get_nome_prof(id_prof:str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""SELECT CONCAT(primeiro_nome, ' ', segundo_nome) AS nome_completo_professor
+                FROM Professor
+                WHERE id_professor = {id_prof};"""
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+#TO-DO
+@professor_blueprint.route('/agendar_chamada', methods=['POST'])
+def agendar_chamada():
+    # Apenas placeholders em lista
+    start_time = request.json.get('start_time')
+    end_time = request.json.get('end_time')
+    chamadas.professor_blueprintend({"start_time": start_time, "end_time": end_time})
+    return jsonify({"status": "success", "message": "chamada agendada com sucesso"}), 200
+
+@professor_blueprint.route('/visualizar_chamada/<string:id_turma>/', methods=['GET'])
+def visualizar_chamada(id_turma: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""SELECT CONCAT(al.primeiro_nome, ' ', al.segundo_nome) as nome, al.matricula, COALESCE(p.id_presenca, 0) as id_presenca
+                FROM Aula a
+                INNER JOIN Turma t ON a.id_turma = t.id_turma
+                INNER JOIN Inscricao i ON t.id_turma = i.id_turma
+                INNER JOIN Aluno al ON i.id_aluno = al.id_aluno
+                LEFT JOIN Presencas p ON a.id_aula = p.id_aula AND al.id_aluno = p.id_aluno
+                WHERE t.id_turma = {id_turma}
+                AND a.situacao = 1 
+                ORDER BY al.id_aluno, a.id_aula;"""
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+@professor_blueprint.route('/chamada_passada/<string:id_turma>/<string:data>', methods=['GET'])
+def chamada_passada(id_turma: str, data:str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""SELECT 
+    CONCAT(al.primeiro_nome, ' ', al.segundo_nome) AS Aluno, 
+    al.matricula,
+    CASE 
+        WHEN p.id_aluno IS NOT NULL THEN 'Presente'
+        ELSE 'Ausente' 
+    END AS presenca
+FROM 
+    Aula a
+JOIN 
+    Turma t ON a.id_turma = t.id_turma
+JOIN 
+    Inscricao i ON t.id_turma = i.id_turma
+JOIN 
+    Aluno al ON i.id_aluno = al.id_aluno
+LEFT JOIN 
+    Presencas p ON a.id_aula = p.id_aula AND al.id_aluno = p.id_aluno
+WHERE 
+    a.id_turma = {id_turma} 
+    AND DATE(a.data_hora_inicio) = '{data}';"""
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+
+@professor_blueprint.route('/get_numero_alunos/<string:id_turma>', methods=['GET'])
+def get_numero_alunos(id_turma: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""SELECT COUNT(id_aluno) AS numero_de_alunos_inscritos
+                FROM Inscricao
+                WHERE id_turma = {id_turma};"""
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+@professor_blueprint.route('/get_percentual_presenca_turma/<string:id_turma>', methods=['GET'])
+def get_percentual_presenca_turma(id_turma: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""SELECT 
+    ROUND(COUNT(p.id_presenca) * 100.0 / COUNT(i.id_aluno), 2) AS Percentual_Presença
+FROM 
+    Aula a
+INNER JOIN 
+    Turma t ON a.id_turma = t.id_turma
+INNER JOIN 
+    Inscricao i ON t.id_turma = i.id_turma
+LEFT JOIN 
+    Presencas p ON a.id_aula = p.id_aula AND i.id_aluno = p.id_aluno
+WHERE 
+    a.id_turma = {id_turma}
+GROUP BY 
+    a.id_aula;"""
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+@professor_blueprint.route('/estatisticas_prof/<string:id_turma>/', methods=['GET'])
+def estatisticas_prof(id_turma: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""
+                SELECT 
+                    CONCAT(A.primeiro_nome, ' ', A.segundo_nome) AS Nome_Aluno,
+                    A.matricula AS Matricula_Aluno,
+                    IFNULL(ROUND((COUNT(P.id_presenca) / COUNT(DISTINCT AU.id_aula)) * 100, 2), 0) AS Porcentagem_Presenca,
+                    SUM(CASE WHEN P.id_presenca IS NOT NULL THEN 1 ELSE 0 END) AS Presencas,
+                    (COUNT(AU.id_aula) - SUM(CASE WHEN P.id_presenca IS NOT NULL THEN 1 ELSE 0 END)) AS Faltas,
+                    CASE 
+                        WHEN (COUNT(AU.id_aula) - SUM(CASE WHEN P.id_presenca IS NOT NULL THEN 1 ELSE 0 END)) >= 8 THEN 'Reprovado por faltas'
+                        ELSE NULL
+                    END AS Situacao
+                FROM 
+                    Aluno A
+                LEFT JOIN 
+                    Inscricao I ON A.id_aluno = I.id_aluno
+                LEFT JOIN 
+                    Turma T1 ON I.id_turma = T1.id_turma
+                LEFT JOIN 
+                    Aula AU ON T1.id_turma = AU.id_turma
+                LEFT JOIN 
+                    Presencas P ON AU.id_aula = P.id_aula AND A.id_aluno = P.id_aluno
+                WHERE 
+                    T1.id_turma = {id_turma}
+                GROUP BY 
+                    A.id_aluno, Nome_Aluno, Matricula_Aluno
+
+                UNION
+
+                SELECT 
+                    CONCAT(AL.primeiro_nome, ' ', AL.segundo_nome) AS Nome_Aluno,
+                    AL.matricula AS Matricula_Aluno,
+                    0 AS Porcentagem_Presenca,
+                    SUM(CASE WHEN PR.id_presenca IS NOT NULL THEN 1 ELSE 0 END) AS Presencas,
+                    (COUNT(AU2.id_aula) - SUM(CASE WHEN PR.id_presenca IS NOT NULL THEN 1 ELSE 0 END)) AS Faltas,
+                    CASE 
+                        WHEN (COUNT(AU2.id_aula) - SUM(CASE WHEN PR.id_presenca IS NOT NULL THEN 1 ELSE 0 END)) >= 8 THEN 'Reprovado por faltas'
+                        ELSE NULL
+                    END AS Situacao
+                FROM 
+                    Aluno AL
+                LEFT JOIN 
+                    Inscricao INSC ON AL.id_aluno = INSC.id_aluno
+                LEFT JOIN 
+                    Aula AU2 ON INSC.id_turma = AU2.id_turma
+                LEFT JOIN 
+                    Presencas PR ON AU2.id_aula = PR.id_aula AND AL.id_aluno = PR.id_aluno
+                WHERE 
+                    INSC.id_turma = {id_turma}
+                GROUP BY 
+                    AL.id_aluno, Nome_Aluno, Matricula_Aluno
+                ORDER BY 
+                    Nome_Aluno, Matricula_Aluno;
+
+
+                """
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+
+
+@professor_blueprint.route('/iniciar_chamada/<string:id_turma>/', methods=['POST'])
+def iniciar_chamada(id_turma: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    localizacao = request.json.get('localizacao')
+    comando = f"""INSERT INTO Aula (id_turma, localizacao, data_hora_inicio, situacao) VALUES
+                ({id_turma}, "{localizacao}", NOW(), 1)"""
+    cursor.execute(comando)
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+    return jsonify({"status": "success", "message": "chamada started"}), 200
+
+@professor_blueprint.route('/finalizar_chamada/<string:id_turma>', methods=['POST'])
+def finalizar_chamada(id_turma:str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""UPDATE Aula 
+                SET situacao = 0, data_hora_fim = NOW() 
+                WHERE id_turma = {id_turma} AND situacao = 1;"""
+    cursor.execute(comando)
+    conexao.commit()
+    cursor.close()
+    conexao.close()
+    return jsonify({"status": "success", "message": "chamada ended"}), 200
+
+
+@professor_blueprint.route('/get_nome_matricula_professor/<string:id_professor>', methods=['GET'])
+def get_nome_matricula_professor(id_professor: str):
+    conexao = open_conexao()
+    cursor = conexao.cursor()
+    comando = f"""
+            SELECT CONCAT(primeiro_nome, ' ', segundo_nome) as Nome, matricula
+            FROM Professor 
+            WHERE id_professor = {id_professor};
+                """
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return jsonify(resultado), 200
+
+
+if __name__ == '__main__':
+    professor_blueprint.run(debug=True)
---
+++ lib/view/home_professor.dart
@@ -0,0 +1,152 @@
+import 'dart:convert';
+import 'package:chamada_inteligente/view/login.dart';
+import 'package:chamada_inteligente/view/view_turma.dart';
+import 'package:flutter/material.dart';
+import 'package:chamada_inteligente/styles/theme_colors.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class HomeProfessor extends StatefulWidget {
+  static String routeName = "/homeprofessor";
+  final int id_professor;
+  HomeProfessor({required this.id_professor});
+
+  @override
+  State<HomeProfessor> createState() => _HomeProfessorState(id_professor: id_professor);
+}
+
+class _HomeProfessorState extends State<HomeProfessor> {
+  // Lista de turmas para demonstração
+  final int id_professor;
+  _HomeProfessorState({required this.id_professor});
+  var env_url = dotenv.env['URL'];
+  List<dynamic> turmasBD = [];  // Lista para armazenar as turmas recebidas do BD
+  String nome_professor = "";
+  String matricula_professor = "";
+  
+  Future<List<dynamic>> _getTurmas() async {
+    var url = Uri.http('${env_url}', '/get_turmas_prof/$id_professor');
+    var response = await http.get(url);
+    List<dynamic> responseData = json.decode(response.body);
+    for (var turma in responseData) {
+      List temp = [];
+      temp.add(turma[0]);
+      temp.add(turma[1]);
+      temp.add(turma[2]);
+      turmasBD.add(temp);
+    }
+
+    var url8 = Uri.http('${env_url}', '/get_nome_matricula_professor/$id_professor');
+    var response2 = await http.get(url8);
+    List<dynamic> responseData2 = json.decode(response2.body);
+    nome_professor = responseData2[0][0];
+    matricula_professor = "${responseData2[0][1]}";
+
+    return responseData;
+  }
+ 
+@override
+Widget build(BuildContext context) {
+  return FutureBuilder<List<dynamic>>(
+    future: _getTurmas(),
+    builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Color(0xFF005AAA),
+          ),
+          body: Center(child: CircularProgressIndicator()),
+        );
+      } else {
+        if (snapshot.hasError) {
+          return Text('Erro: ${snapshot.error}');
+        } else {
+          turmasBD = snapshot.data!;
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              backgroundColor: Color(0xFF005AAA),
+              leading: IconButton(
+                icon: Icon(Icons.logout, color: Colors.white, size: 40),
+                onPressed: () {
+                  _logout(context);
+                },
+              ),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.list_alt, color: Colors.white, size: 30),
+                  SizedBox(width: 10),                 
+                  Padding(
+                    padding: EdgeInsets.only(right: 60), // Adiciona margem à esquerda do texto
+                    child: Text(
+                      'Turmas',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),              
+            ),
+            body: ListView.builder(
+              itemCount: turmasBD.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Color(0xFFFbbc4e3),
+                  child: ListTile(
+                    title: Text(
+                      '${turmasBD[index][0]}',
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      'Turma: ${turmasBD[index][1]}',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              TurmaPage(disciplina: turmasBD[index][0]!, codTurma: turmasBD[index][1]!, id_turma: turmasBD[index][2]!, id_professor: id_professor),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+            bottomNavigationBar: BottomAppBar(
+        child: Container(
+          color: Color(0xFF005AAA),
+          height: 30.0,
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('Professor: ${nome_professor}',style: TextStyle(color: Colors.white,fontSize: 16.0),),
+              Text('Matrícula: ${matricula_professor}',style: TextStyle(color: Colors.white,fontSize: 16.0),),
+            ],
+          ),
+        ),
+      ),
+          );
+        }
+      }
+    },
+  );
+}
+
+void _logout(BuildContext context) async {
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  await preferences.clear();
+
+  Navigator.pushAndRemoveUntil(
+    context,
+    MaterialPageRoute(
+      builder: (BuildContext context) => LoginPage(),
+    ),
+    (route) => false,
+  );
+}
+}
