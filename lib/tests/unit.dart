3851163f345a3bbccfa90aa9d904cf005dd827df
Rafael Aguiar Martins <rafael_aguiar@id.uff.br>
2023-11-28 00:12:26 -0300

    teste unitario loginpage

--- lib/tests/unit.dart
+++ lib/tests/unit.dart
@@ -1,60 +1,75 @@
+import 'dart:convert';
 import 'package:chamada_inteligente/view/home_aluno.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 import 'package:chamada_inteligente/view/login.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 
-class MockClient extends Mock implements http.Client {}
-
+class MockClient extends Mock implements http.Client {
+  @override
+  Future<http.Response> post(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    return super.noSuchMethod(
+      Invocation.method(#post, [url], {#headers: headers, #body: body, #encoding: encoding}),
+      returnValue: Future.value(http.Response('[[1, "Amara", "Paz", 1001, "senha1", 1]]', 200)),
+      returnValueForMissingStub: Future.value(http.Response('Error', 404)),
+    );
+  }
+}
 void main() {
-  group('Login Functionality', () {
-    testWidgets('Successful login for student', (WidgetTester tester) async {
+
+  group('Login Page Tests', () {
+    testWidgets('Successful login navigates to HomeAluno', (WidgetTester tester) async {
       final client = MockClient();
 
-      // Mock the HTTP response for successful login
 
+      var url = Uri.http('192.168.0.105:5000', '/LoginAluno');
+      // Configuração do mock
       when(client.post(
-        Uri.parse('http://127.0.0.1/LoginAluno'), // Replace with actual URL
+        url,
         headers: anyNamed('headers'),
-        body: anyNamed('body')
-      )).thenAnswer((_) async =>
-          http.Response('[["studentId", "studentName", ...]]', 200)); // Mock response format
+        body: anyNamed('body'),
+      )).thenAnswer((_) async => http.Response('[[1, "Amara", "Paz", 1001, "senha1", 1]]', 200));
 
       await tester.pumpWidget(MaterialApp(home: LoginPage()));
 
-      // Simulate user input
-      await tester.enterText(find.byKey(const Key('matriculaField')), '2001');
-      await tester.enterText(find.byKey(const Key('passwordField')), 'senha1');
-      await tester.tap(find.byKey(const Key('loginButton')));
-      await tester.pumpAndSettle();
+      
+      await tester.enterText(find.widgetWithText(FormInput, 'Usuário:'), '2001');
+      await tester.enterText(find.widgetWithText(FormInput, 'Senha:'), 'senha1');
 
-      // Verify navigation
-      expect(find.byType(HomeAluno), findsOneWidget); // Verify that it navigates to the student home page
+      
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      
+      // Checar se navegou para HomeAluno, que tem Icons
+      expect(find.byType(Icon), findsWidgets);
     });
 
-    testWidgets('Failed login attempt', (WidgetTester tester) async {
+    testWidgets('Unsuccessful login shows error', (WidgetTester tester) async {
+      // Mock the HTTP client
       final client = MockClient();
-      const loginPage = LoginPage();
 
-      // Mock the HTTP response for failed login
+      var url = Uri.http('192.168.0.105:5000', '/LoginAluno');
+      // Mock failed login response
       when(client.post(
-        Uri.parse('http://127.0.0.1/LoginAluno'), // Replace with actual URL
+        url,
         headers: anyNamed('headers'),
-        body: anyNamed('body')
-      )).thenAnswer((_) async =>
-          http.Response('[]', 200)); // Empty response indicating failed login
+        body: anyNamed('body'),
+      )).thenAnswer((_) async => http.Response('{"error": "Invalid credentials"}', 401));
 
-      await tester.pumpWidget(MaterialApp(home: loginPage));
+      // Render the LoginPage
+      await tester.pumpWidget(MaterialApp(home: LoginPage()));
 
-      // Simulate user input
-      await tester.enterText(find.byKey(const Key('matriculaField')), '5555');
-      await tester.enterText(find.byKey(const Key('passwordField')), 'wrongPassword');
-      await tester.tap(find.byKey(const Key('loginButton')));
+      // Enter credentials
+      await tester.enterText(find.widgetWithText(FormInput, 'Usuário:'), 'wrongUser');
+      await tester.enterText(find.widgetWithText(FormInput, 'Senha:'), 'wrongPassword');
+
+      // Tap the login button
+      await tester.tap(find.byType(ElevatedButton));
       await tester.pumpAndSettle();
 
-      // Verify that it does not navigate to the student home page
-      expect(find.byType(HomeAluno), findsNothing);
+      // Check if error dialog is shown
+      expect(find.text('Login Inválido!\n\nVerifique Usuário e Senha!'), findsOneWidget);
     });
   });
 }
